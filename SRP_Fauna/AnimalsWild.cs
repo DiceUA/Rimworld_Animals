f8b7576b86a2654c9e501d3bc10b2bb1c532f7c7
Nyaka <pillar.katter@gmail.com>
2015-03-05 23:39:13 +0200

    Animals list DONE

---
+++ SRP_Fauna/AnimalsWild.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SRP_Fauna
+{
+    public enum AnimalsWild : int
+    {
+        Muffalo,
+        Deer,
+        Squirrel,
+        Boomrat,
+        Dromedary,
+        Monkey,
+        Rhinoceros,
+        Boar,
+        Furx,
+        Rimdog,
+        Rimram,
+        Rimwolf,
+        Snork,
+        Megascarab,
+        Iguana,
+        Cobra,
+        Tortoise,
+        Lacosdile,
+        Megaslug
+    }
+}
