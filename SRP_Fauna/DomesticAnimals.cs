d45195f70c8127e25ab5b48d6d35b67f3f4fe952
DiceUA <pillar.katter@gmail.com>
2015-03-05 17:23:58 +0200

    Making enums for Domestic Animals

---
+++ SRP_Fauna/DomesticAnimals.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SRP_Fauna
+{
+    /// <summary>
+    /// Add new animals here
+    /// </summary>
+ 
+    enum DomesticAnimals
+    {
+        MuffaloDomestic,
+        DeerDomestic,
+        SquirrelDomestic,
+        BoomratDomestic,
+        DromedaryDomestic,
+        MonkeyDomestic,
+        RhinocerosDomestic,
+        BoarDomestic,
+        FurxDomestic,
+        RimdogDomestic,
+        RimramDomestic,
+        RimwolfDomestic,
+        SnorkDomestic
+    }
+}
---
+++ SRP_Fauna/DomesticEggLayers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SRP_Fauna
+{
+    public enum DomesticEggLayers
+    {
+        MegascarabDomestic,
+        IguanaDomestic,
+        CobraDomestic,
+        TortoiseDomestic,
+        LacosdileDomestic,
+        MegaslugDomestic
+    }
+}
