e7986af30dab2b46f61c31156e76b39867027f8b
DiceUA <pillar.katter@gmail.com>
2015-03-06 14:33:31 +0200

    Class for lists

---
+++ SRP_Fauna/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SRP_Fauna
+{
+    public class Animal
+    {
+        private string name;
+        private string species;
+        private bool eggLayer;
+
+        public string Species
+        {
+            get { return species; }
+            set { species = value; }
+        }
+
+        public bool EggLayer
+        {
+            get { return eggLayer; }
+            set {
+                if (Enum.IsDefined(typeof(DomesticEggLayers), value + "Domestic"))
+                    eggLayer = true;
+                else
+                    eggLayer = false;           
+            }
+        }
+
+        private bool IsEggLayer (string animalWild)
+        {
+
+            if (Enum.IsDefined(typeof(DomesticEggLayers), animalWild + "Domestic" ))
+                return true;
+            else
+                return false;
+        }
+    }
+}
